60cfaca216bb4f244d6f47f8e8e29a76ed522f4d
sajal chaurasia <sajalchaurasia@sajals-MacBook-Pro.local>
2026-03-02 09:14:13 +0100

    changing task 3 md_datasets and gnn_utils file

--- Task_3_Data_Processing/md_datasets.py
+++ Task_3_Data_Processing/md_datasets.py
@@ -5,7 +5,7 @@ import pickle
 
 import torch
 from torch.utils.data import Dataset
-from torch_geometric.data import Batch
+from torch_geometric.data import Batch, HeteroData
 
 from joblib import Parallel, delayed
 
@@ -66,23 +66,90 @@ class MDTrajDataset(Dataset):
 
             # Build distance graph
             # distance_threshold       # Adjust as needed
-            graphs = Parallel(n_jobs=-1)(
+            all_graphs = Parallel(n_jobs=-1)(
                     delayed(build_frame_graph)(
+                        atoms_trajectories[t, :, :][selected_atom_indices],
                         atoms_trajectories[t, :, :][selected_atom_indices], 
-                        np.ones(len(selected_atom_indices)), 
+                        np.ones((len(selected_atom_indices), 1)), 
+                        self.k, 
+                        self.distance_threshold, 
+                        self.graph_type)
+                    for t in range(self.T)
+                    )
+            all_batched_graph = Batch.from_data_list(all_graphs)
+
+            # Hetero Graph construction
+            prot_prot_graphs = Parallel(n_jobs=-1)(
+                    delayed(build_frame_graph)(
+                        atoms_trajectories[t, :, :][binding_site_atom_indices],
+                        atoms_trajectories[t, :, :][binding_site_atom_indices],
+                        np.ones((len(binding_site_atom_indices), 1)), 
+                        self.k, 
+                        self.distance_threshold, 
+                        self.graph_type)
+                    for t in range(self.T)
+                    )
+            
+            lig_lig_graphs = Parallel(n_jobs=-1)(
+                    delayed(build_frame_graph)(
+                        atoms_trajectories[t, :, :][ligand_atoms_indices],
+                        atoms_trajectories[t, :, :][ligand_atoms_indices],
+                        np.ones((len(ligand_atoms_indices), 1)), 
                         self.k, 
                         self.distance_threshold, 
                         self.graph_type)
                     for t in range(self.T)
                     )
 
-            batched_graph = Batch.from_data_list(graphs)
+            prot_lig_graphs = Parallel(n_jobs=-1)(
+                    delayed(build_frame_graph)(
+                        atoms_trajectories[t, :, :][binding_site_atom_indices],
+                        atoms_trajectories[t, :, :][ligand_atoms_indices],
+                        1, 
+                        self.k, 
+                        self.distance_threshold, 
+                        self.graph_type)
+                    for t in range(self.T)
+                    )
+            
+            hetero_graphs_list = []
+
+            for t in range(self.T):
+                data = HeteroData()
+                data['protein'].x = prot_prot_graphs[t].x
+                data['ligand'].x = lig_lig_graphs[t].x
+
+                data['protein', 'bond', 'protein'].edge_index = prot_prot_graphs[t].edge_index
+                data['protein', 'bond', 'protein'].edge_weight = prot_prot_graphs[t].edge_attr
+
+                data['ligand', 'bond', 'ligand'].edge_index = lig_lig_graphs[t].edge_index
+                data['ligand', 'bond', 'ligand'].edge_weight = lig_lig_graphs[t].edge_attr
+
+                data['protein', 'interacts', 'ligand'].edge_index = prot_lig_graphs[t].edge_index
+                data['protein', 'interacts', 'ligand'].edge_weight = prot_lig_graphs[t].edge_attr
+
+                data['ligand', 'interacts', 'protein'].edge_index = torch.flip(data['protein', 'interacts', 'ligand'].edge_index, [0])
+                data['ligand', 'interacts', 'protein'].edge_weight = data['protein', 'interacts', 'ligand'].edge_weight
+
+                hetero_graphs_list.append(data)
+
+            hetero_batched_graph = Batch.from_data_list(hetero_graphs_list)
 
             data_dict = {
-                        "edge_index": batched_graph.edge_index.to(torch.int32),
-                        "edge_attr": batched_graph.edge_attr,
-                        "batch": batched_graph.batch.to(torch.int32),
-                        "ptr": batched_graph.ptr.to(torch.int32)
+                        "protein_protein_edge_index": hetero_batched_graph[('protein', 'bond', 'protein')]['edge_index'].to(torch.int32),
+                        "protein_protein_edge_attr": hetero_batched_graph[('protein', 'bond', 'protein')]['edge_weight'],
+                        "protein_protein_batch": hetero_batched_graph['protein']['batch'].to(torch.int32),
+                        "protein_protein_ptr": hetero_batched_graph['protein']['ptr'].to(torch.int32),
+                        "ligand_ligand_edge_index":hetero_batched_graph[('ligand', 'bond', 'ligand')]['edge_index'].to(torch.int32),
+                        "ligand_ligand_edge_attr":hetero_batched_graph[('ligand', 'bond', 'ligand')]['edge_weight'],
+                        "ligand_ligand_batch":hetero_batched_graph['ligand']['batch'].to(torch.int32),
+                        "ligand_ligand_ptr":hetero_batched_graph['ligand']['ptr'].to(torch.int32),
+                        "protein_ligand_edge_index":hetero_batched_graph[('protein', 'interacts', 'ligand')]['edge_index'].to(torch.int32),
+                        "protein_ligand_edge_attr":hetero_batched_graph[('protein', 'interacts', 'ligand')]['edge_weight'],
+                        "all_edge_index": all_batched_graph.edge_index.to(torch.int32),
+                        "all_edge_attr": all_batched_graph.edge_attr,
+                        "all_batch": all_batched_graph.batch.to(torch.int32),
+                        "all_ptr": all_batched_graph.ptr.to(torch.int32)
                         }
 
             torch.save(data_dict, os.path.join(self.processed_dir, f'{protein_key}.pt'))
@@ -120,25 +187,76 @@ class MDTrajDataset(Dataset):
         selected_atom_indices = np.concatenate([binding_site_atom_indices, ligand_atoms_indices])
 
         # node_features = atom type + charge + ligand or not
-        node_features = torch.as_tensor(self.preprocessed_graphs_H5File[protein_key]['atom_1hot'][()])
+        one_hot_atom_features = torch.as_tensor(self.preprocessed_graphs_H5File[protein_key]['atom_1hot'][()])
+        one_hot_atom_protein = one_hot_atom_features[:len(binding_site_atom_indices), :]
+        one_hot_atom_ligand = one_hot_atom_features[len(binding_site_atom_indices):, :]
+
         charges = normalized(torch.as_tensor(self.preprocessed_graphs_H5File[protein_key]['charges'][()]))
-        lig_index = self.preprocessed_graphs_H5File[protein_key]["ligand_begin_index"][()]
-        lig_len = int(charges.shape[0]) - lig_index
-        nodes_numlig = torch.cat((torch.zeros(lig_index), torch.ones(lig_len))).unsqueeze(1)
-        node_features = torch.cat((node_features, charges, nodes_numlig), 1)
+        charges_protein = charges[:len(binding_site_atom_indices), :]
+        charges_ligand = charges[len(binding_site_atom_indices):, :]
 
-        # Load the graph data
+        node_features_protein = torch.cat((one_hot_atom_protein, charges_protein, torch.zeros(one_hot_atom_protein.shape[0]).unsqueeze(1)), 1)
+        node_features_ligand = torch.cat((one_hot_atom_ligand, charges_ligand, torch.ones(one_hot_atom_ligand.shape[0]).unsqueeze(1)), 1)
+
+        #------ Load the Hetero Batched Graph data ------#
+    
+        # Create the dummy Hetero Batched Graph
+        data = HeteroData()
+        data['protein'].x = torch.ones((100,1))
+        data['ligand'].x = torch.ones((20,1))
+
+        data['protein', 'bond', 'protein'].edge_index = torch.randint(0, 100, (2, 2))
+        data['protein', 'bond', 'protein'].edge_weight = torch.rand(2) 
+
+        data['ligand', 'bond', 'ligand'].edge_index = torch.randint(0, 20, (2, 2))
+        data['ligand', 'bond', 'ligand'].edge_weight = torch.rand(2)
+
+        src_p = torch.randint(0, 100, (1, 2))
+        dst_l = torch.randint(0, 20, (1, 2))
+        data['protein', 'interacts', 'ligand'].edge_index = torch.cat([src_p, dst_l], dim=0)
+        data['protein', 'interacts', 'ligand'].edge_weight = torch.rand(2)
+
+        data['ligand', 'interacts', 'protein'].edge_index = torch.flip(data['protein', 'interacts', 'ligand'].edge_index, [0])
+        data['ligand', 'interacts', 'protein'].edge_weight = data['protein', 'interacts', 'ligand'].edge_weight
+
+        hetero_batched_graph = Batch.from_data_list([data, data])  # Dummy batch of size 2
+
+        # Load the graph data from disk
         data_dict = torch.load(os.path.join(self.processed_dir, f'{protein_key}.pt'))
-        batched_graph = Batch()
-        batched_graph.edge_index = data_dict['edge_index'].to(torch.int64)
-        batched_graph.ptr = data_dict['ptr'].to(torch.int64)
-        batched_graph.batch = data_dict['batch'].to(torch.int64)
-        batched_graph.edge_attr = data_dict['edge_attr']
-        batched_graph.pos = torch.tensor(atoms_trajectories[:, selected_atom_indices, :].reshape(-1, 3), dtype=torch.float32)
-        batched_graph.x = torch.tile(node_features, (self.T, 1))
 
+        # Assign the loaded data to the Hetero Batched Graph
+        hetero_batched_graph['protein'].x = torch.tile(node_features_protein, (self.T, 1))
+        hetero_batched_graph['protein'].batch = data_dict['protein_protein_batch'].to(torch.int64)
+        hetero_batched_graph['protein'].ptr = data_dict['protein_protein_ptr'].to(torch.int64)
+
+        hetero_batched_graph['ligand'].x = torch.tile(node_features_ligand, (self.T, 1))
+        hetero_batched_graph['ligand'].batch = data_dict['ligand_ligand_batch'].to(torch.int64)
+        hetero_batched_graph['ligand'].ptr = data_dict['ligand_ligand_ptr'].to(torch.int64)
+
+        hetero_batched_graph[('protein', 'bond', 'protein')].edge_index = data_dict['protein_protein_edge_index'].to(torch.int64)
+        hetero_batched_graph[('protein', 'bond', 'protein')].edge_weight = data_dict['protein_protein_edge_attr']
+
+        hetero_batched_graph[('ligand', 'bond', 'ligand')].edge_index = data_dict['ligand_ligand_edge_index'].to(torch.int64)
+        hetero_batched_graph[('ligand', 'bond', 'ligand')].edge_weight = data_dict['ligand_ligand_edge_attr']
+
+        hetero_batched_graph[('protein', 'interacts', 'ligand')].edge_index = data_dict['protein_ligand_edge_index'].to(torch.int64)
+        hetero_batched_graph[('protein', 'interacts', 'ligand')].edge_weight = data_dict['protein_ligand_edge_attr']
+
+        hetero_batched_graph[('ligand', 'interacts', 'protein')].edge_index = torch.flip(hetero_batched_graph['protein', 'interacts', 'ligand'].edge_index, [0])
+        hetero_batched_graph[('ligand', 'interacts', 'protein')].edge_weight = hetero_batched_graph['protein', 'interacts', 'ligand'].edge_weight
+
+        #------ Load the All Batched Graph data ------#
+        all_batched_graph = Batch()
+        all_batched_graph.edge_index = data_dict['all_edge_index'].to(torch.int64)
+        all_batched_graph.ptr = data_dict['all_ptr'].to(torch.int64)
+        all_batched_graph.batch = data_dict['all_batch'].to(torch.int64)
+        all_batched_graph.edge_attr = data_dict['all_edge_attr']
+        all_batched_graph.pos = torch.tensor(atoms_trajectories[:, selected_atom_indices, :].reshape(-1, 3), dtype=torch.float32)
+
+        #------ Prepare the target ------#
         target = torch.tensor(self.preprocessed_graphs_H5File[protein_key]['-logKd_Ki'][()], dtype=torch.float32) # (1,)
-        return batched_graph, target
+
+        return hetero_batched_graph, all_batched_graph, target
 
     def __len__(self):
         return len(self.datatype_ids)
