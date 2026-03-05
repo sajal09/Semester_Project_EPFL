adec0b3c21391473264348917b83a55ad464c1a5
sajal chaurasia <sajalchaurasia@sajals-MacBook-Pro.local>
2026-03-05 09:39:56 +0100

    adding zero protein atom ids removal code in task 3 part B

--- Task_3_Data_Processing_2/train_val_test_ids.py
+++ Task_3_Data_Processing_2/train_val_test_ids.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import h5py
+import numpy as np
 import os
 import pickle
 import random
+from md_datasets import create_dataset
+from torch.utils.data import DataLoader
 
 affinity_df = pd.read_csv("/work/lts2/users/sajal/data/task3/all_dataset_merged.csv")
 general_refined_set = list(affinity_df[affinity_df.Set == 'general_refined']['PDB_code']) # 5050 ids
@@ -60,6 +63,41 @@ for protein_key in md_H5File:
 f_train = [i for i in f_train if i not in bad_ids]
 f_val = [i for i in f_val if i not in bad_ids]
 f_test = [i for i in f_test if i not in bad_ids]
+
+# There are some bad ids in which there is no protein atom which is less than 4.5A distance from the ligand.
+# Such examples have zero protein atoms in the protein-protein graph which causes NaN loss during training.
+# We remove them from the train, test, val sets.
+k = 2
+distance_threshold = 4.5
+graph_type = 'threshold'
+folder_path = '/work/lts2/users/sajal/data/task3/task3_full_data'
+T = 100
+train_dataset = create_dataset('train', k, distance_threshold, graph_type, os.path.join(folder_path, 'train_data'), T)
+train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=lambda x: x[0])
+val_dataset = create_dataset('val', k, distance_threshold, graph_type, os.path.join(folder_path, 'val_data'), T)
+val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=lambda x: x[0])
+test_dataset = create_dataset('test', k, distance_threshold, graph_type, os.path.join(folder_path, 'test_data'), T)
+test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=lambda x: x[0])
+
+problematic_ids = []
+for i in range(len(train_dataloader)):
+    hetero_graph, batched_graph, target = train_dataset.__getitem__(i)
+    if hetero_graph['protein'].x.shape[0] == 0:
+        problematic_ids.append(train_dataset.datatype_ids[i])
+
+for i in range(len(val_dataloader)):
+    hetero_graph, batched_graph, target = val_dataset.__getitem__(i)
+    if hetero_graph['protein'].x.shape[0] == 0:
+        problematic_ids.append(val_dataset.datatype_ids[i])
+
+for i in range(len(test_dataloader)):
+    hetero_graph, batched_graph, target = test_dataset.__getitem__(i)
+    if hetero_graph['protein'].x.shape[0] == 0:
+        problematic_ids.append(test_dataset.datatype_ids[i])
+
+f_train = [i for i in f_train if i not in problematic_ids] # 4DHL, 4MMM, 4K7N removed
+f_val = [i for i in f_val if i not in problematic_ids] # None removed
+f_test = [i for i in f_test if i not in problematic_ids] # None removed
 
 with open('train_ids.txt', 'w') as f:
     f.write('\n'.join(f_train))
